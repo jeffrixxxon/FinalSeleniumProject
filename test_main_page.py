4d30e0e3e18622dda898e3a5465ffc4605207a3a
daniil <jeffrixxxon@mail.ru>
2023-04-01 17:19:23 +0300

    Adding a login page test

--- test_main_page.py
+++ test_main_page.py
@@ -15,3 +15,11 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
+
+def test_guest_go_to_login_page(browser):
+    page = MainPage(browser, LINK)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
