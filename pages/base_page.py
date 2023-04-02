cd50ab835cd3c09dd6f9b985f94fbd3e6f5f23e3
daniil <jeffrixxxon@mail.ru>
2023-04-02 18:21:40 +0300

    Transferred function go_to_login_page and should_be_login_link in base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,5 @@
 import typing
+from .locators import BasePageLocators
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,6 +12,18 @@ class BasePage:
         self.browser: webdriver = browser
         self.url: str = url
         #self.browser.implicitly_wait(timeout)
+
+    def go_to_login_page(self):
+        try:
+            link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+            link.click()
+        except NoSuchElementException:
+            assert False, 'NoSuchElementException: element is missing from the page'
+        else:
+            assert 'login' in self.browser.current_url, 'You are not on the login page'
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def open(self) -> typing.NoReturn:
         self.browser.get(self.url)
@@ -33,8 +46,8 @@ class BasePage:
 
     def is_disappeared(self, how, what, timeout=4):
         try:
-            WebDriverWait(self.browser, timeout, 1, ignored_exceptions=[TimeoutException]). \
-                until_not(EC.presence_of_element_located((how, what)))
+            WebDriverWait(self.browser, timeout, poll_frequency=1, ignored_exceptions=[TimeoutException]
+                          ).until_not(EC.presence_of_element_located((how, what)))
         except TimeoutException:
             return False
 
