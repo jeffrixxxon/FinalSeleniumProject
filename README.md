18abfe37b583c9e9fe83f5cfd07b22af6b04137b
daniil <jeffrixxxon@mail.ru>
2023-04-03 21:13:18 +0300

    Minor changes

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# FinalAutoTestesProject
+# FinalAutoTestesProject по <a href='https://stepik.org/course/575/info'>курсу.</a>
+
+## Установка:
+
+<div>
+
+    git clone git@github.com:jeffrixxxon/FinalSeleniumProject.git
+
+</div>
+
+<div>
+
+    cd FinalSeleniumProject
+
+</div>
+
+<div>
+    
+    pip install -r requirements.txt
+
+</div>
+
+## Запуск:
+
+<div>
+
+    pytest -s -v --tb=line test_product_page.py
+
+</div>
+
+### Или:
+
+<div>
+
+    pytest -s -v --tb=line test_main_page.py
+
+</div>
+
+## Опцианально можно выбрать язык страницы:
+> по умолчанию стоит en: English
+
+<div>
+
+    pytest -s -v --tb=line --language=язык название_теста
+
+</div>
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,25 +1,25 @@
 import math
-import typing
+from typing import NoReturn
 from selenium.common.exceptions import NoAlertPresentException, NoSuchElementException
 from .base_page import BasePage
 from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
-    def should_not_be_success_message(self):
+    def should_not_be_success_message(self) -> NoReturn:
         assert self.is_not_element_present(*ProductPageLocators.MESSAGE_ABOUT_ADDING), (
             "Success message is presented, but should not be")
 
-    def should_disappear_success_message(self):
+    def should_disappear_success_message(self) -> NoReturn:
         assert self.is_disappeared(*ProductPageLocators.MESSAGE_ABOUT_ADDING), (
             "The error message hasn't gone away, but it should"
         )
 
-    def press_button_add_to_basket(self) -> typing.NoReturn:
+    def press_button_add_to_basket(self) -> NoReturn:
         button_add_to_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
         button_add_to_basket.click()
 
-    def should_be_message_about_adding(self) -> typing.NoReturn:
+    def should_be_message_about_adding(self) -> NoReturn:
         assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), (
             "Product name is not presented")
         assert self.is_element_present(*ProductPageLocators.MESSAGE_ABOUT_ADDING), (
@@ -28,7 +28,7 @@ class ProductPage(BasePage):
         message = self.browser.find_element(*ProductPageLocators.MESSAGE_ABOUT_ADDING).text
         assert product_name == message, "No product name in the message"
 
-    def should_be_message_basket_total(self) -> typing.NoReturn:
+    def should_be_message_basket_total(self) -> NoReturn:
         assert self.is_element_present(
             *ProductPageLocators.MESSAGE_BASKET_TOTAL), "Message basket total is not presented"
         assert self.is_element_present(
@@ -37,10 +37,10 @@ class ProductPage(BasePage):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         assert product_price == message_basket_total, "No product price in the message"
 
-    def solve_quiz_and_get_code(self) -> typing.NoReturn:
+    def solve_quiz_and_get_code(self) -> NoReturn:
         alert = self.browser.switch_to.alert
-        x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        search_num = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(search_num))))))
         alert.send_keys(answer)
         alert.accept()
         try:
