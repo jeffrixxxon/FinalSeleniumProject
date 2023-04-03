96dfd3ce8c9292e41ac014fb920fd292ce532f7e
daniil <jeffrixxxon@mail.ru>
2023-04-03 21:14:18 +0300

    Combined the login verification form into a class under the label login_guest

--- test_main_page.py
+++ test_main_page.py
@@ -9,16 +9,17 @@ from pages.basket_page import BasketPage
 LINK = "http://selenium1py.pythonanywhere.com/"
 
 
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, LINK)
-    page.open()
-    page.go_to_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, LINK)
+        page.open()
+        page.go_to_login_page()
 
-
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, LINK)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, LINK)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_go_to_login_page(browser):
