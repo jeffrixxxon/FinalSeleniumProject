212bad2f95df0d7d460864af55b9c6475f755d28
daniil <jeffrixxxon@mail.ru>
2023-04-03 12:23:19 +0300

    Added cart search and cart final cost locators

--- pages/locators.py
+++ pages/locators.py
@@ -2,8 +2,13 @@ from selenium.webdriver.common.by import By
 
 
 class BasePageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK: tuple = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID: tuple = (By.CSS_SELECTOR, "#login_link")
+
+
+class BasketPageLocators:
+    BASKET_POSITION: tuple = (By.CSS_SELECTOR, '.btn-group a')
+    EMPTY_BASKET: tuple = (By.CSS_SELECTOR, '.total')
 
 
 class MainPageLocators:
@@ -16,9 +21,9 @@ class LoginPageLocators:
 
 
 class ProductPageLocators:
-    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
-    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner strong")
-    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
-    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
-    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    BUTTON_ADD_TO_BASKET: tuple = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    MESSAGE_ABOUT_ADDING: tuple = (By.CSS_SELECTOR, "div.alertinner strong")
+    PRODUCT_NAME: tuple = (By.CSS_SELECTOR, ".product_main h1")
+    MESSAGE_BASKET_TOTAL: tuple = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    PRODUCT_PRICE: tuple = (By.CSS_SELECTOR, ".product_main .price_color")
 
