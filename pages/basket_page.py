fb918015d779984f149192c2121182cd88878356
daniil <jeffrixxxon@mail.ru>
2023-04-03 12:22:20 +0300

    Added functions to check that the cart is empty or not empty

---
+++ pages/basket_page.py
@@ -0,0 +1,14 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+    def should_basket_is_empty(self):
+        assert self.is_not_element_present(*BasketPageLocators.EMPTY_BASKET), (
+            'Cart is not empty, but should'
+        )
+
+    def should_basket_is_not_empty(self):
+        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET), (
+            'Cart is empty, but should'
+        )
