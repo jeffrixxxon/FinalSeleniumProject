8e3dd8119bb44d4c6f4014081049897744615fb9
daniil <jeffrixxxon@mail.ru>
2023-04-03 12:31:28 +0300

    Added a test for moving from the main page to the cart and checking that it is empty and not empty

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,10 @@
+import time
+
+import pytest
+
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
 
 LINK = "http://selenium1py.pythonanywhere.com/"
 
@@ -22,4 +27,19 @@ def test_guest_go_to_login_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    page = BasketPage(browser, LINK)
+    page.open()
+    page.go_to_basket_page()
+    page.should_basket_is_empty()
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_product_not_in_basket_opened_from_main_page(browser):
+    page = BasketPage(browser, LINK)
+    page.open()
+    page.go_to_basket_page()
+    page.should_basket_is_not_empty()
 
